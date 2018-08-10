329a767fff9dd943e7a31063a639c65e4e0fae5a
larsp <lars@f-w.dk>
2018-08-10 19:16:51 +0300

    Engine list improved

--- src/enginelist.js
+++ src/enginelist.js
@@ -5,13 +5,13 @@ var searchEngineList = [
 		{
 			key : "STARTPAGE",
 			display_name : "startpage",
-			description : "The worlds most private search engine",
+			description : "startpage web search",
 			query_url : "https://startpage.com/do/search?language=english&cat=web&query=$QUERY",
 			about_url : "https://www.startpage.com/"
 		}, {
 			key : "DUCKDUCKGO",
 			display_name : "DuckDuckGo",
-			description : "The search engine that doesn't track you",
+			description : "DuckDuckGo web search",
 			query_url : "https://duckduckgo.com/?q=$QUERY_PLUS_SEP&ia=web",
 			about_url : "https://duckduckgo.com/"
 		}, {
@@ -45,6 +45,12 @@ var searchEngineList = [
 			key : "YOUTUBE",
 			display_name : "YouTube",
 			query_url : "https://www.youtube.com/results?search_query=$QUERY"
+		}, {
+			key : "IMDB",
+			display_name : "IMDb",
+			description : "Search the Internet Movie Database",
+			query_url : "https://www.imdb.com/find?q=$QUERY_PLUS_SEP",
+			about_url : "https://www.imdb.com"
 		}
 	]
 },
@@ -54,7 +60,7 @@ var searchEngineList = [
 		{
 			key : "URL",
 			display_name : "Open as URL",
-			description : "Use query as URL directly",
+			description : "Open search query as the URL",
 			mode : "URL"
 		}, {
 			key : "WIKIPEDIA",
@@ -63,9 +69,17 @@ var searchEngineList = [
 			query_url : "https://en.wikipedia.org/w/index.php?search=$QUERY_PLUS_SEP",
 			about_url : "https://en.wikipedia.org/wiki/Main_Page"
 		}, {
+			key : "WIKIMEDIA-COMMONS",
+			display_name : "Wikimedia commons",
+			description : "Wikimedia commons search",
+			query_url : "https://commons.wikimedia.org/w/index.php?search=$QUERY_PLUS_SEP",
+			about_url : "https://commons.wikimedia.org"
+		}, {
 			key : "GOOGLE-MAPS",
 			display_name : "Google Maps",
-			query_url : "https://maps.google.com?q=$QUERY"					
+			description : "Search Google Maps",
+			query_url : "https://maps.google.com?q=$QUERY",
+			about_url : "https://maps.google.com"
 		}
 	]	    		
 },
@@ -75,11 +89,26 @@ var searchEngineList = [
 		{	
 			key : "GOOGLE-TRANSLATE",
 			display_name : "Google Translate",
-			query_url : "https://translate.google.com/#auto/en/$QUERY"
+			query_url : "https://translate.google.com/#auto/en/$QUERY",
+			about_url : "https://translate.google.com"
 		}, {
 			key : "THESAURUS.COM",
 			display_name : "thesaurus.com",
-			query_url : "http://thesaurus.com/browse/$QUERY"
+			description : "Search thesaurus.com",
+			query_url : "https://thesaurus.com/browse/$QUERY",
+			about_url : "https://thesaurus.com"
+		}, {
+			key : "MERRIAM-WEBSTERS-DICT",
+			display_name : "Merriam Websters",
+			description : "Search Merriam Websters dictionary",
+			query_url : "https://www.merriam-webster.com/dictionary/$QUERY",
+			about_url : "https://www.merriam-webster.com"
+		}, {
+			key : "MERRIAM-WEBSTERS-THES",
+			display_name : "Merriam Websters Thesaurus",
+			description : "Search Merriam Websters thesaurus",
+			query_url : "https://www.merriam-webster.com/thesaurus/$QUERY",
+			about_url : "https://www.merriam-webster.com"
 		}
 	]	    		
 }];
